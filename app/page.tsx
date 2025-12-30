85cdae3cd5fdb5c54d61651d4e8c085fe131f211
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2025-12-30 21:11:33 +0530

    commit before experimentaion

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,6 @@ import { ModeToggle } from "../components/theme-toggle";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans"></div>
+
   );
 }
