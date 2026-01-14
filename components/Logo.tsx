1e86f6af96978d506e29a676c9b5915fff655e6c
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2026-01-14 18:36:25 +0530

    Refactor Prisma and utils, update imports and aliases
    Moved Prisma client setup from app/lib/prisma.ts to lib/db/prisma.ts with improved logging. Deleted unused utils and example component files. Updated import paths in Logo and Container components to use new alias and location. Cleaned up components.json aliases to remove deprecated entries.

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import React from "react";
-import { cn } from "../app/lib/utils";
+import Link from "next/link"
+import React from "react"
+import { cn } from "@/lib/utils"
 
 export const Logo = ({ className }: { className?: string }) => {
   return (
--- components/container.tsx
+++ components/container.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { cn } from "../lib/utils";
+import React from "react"
+import { cn } from "@/lib/utils"
 // this is the entire code of the container which allow us to put our content in fixed width
 
 export const Container = ({
